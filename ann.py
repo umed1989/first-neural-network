bec58f6ed8c212c4998ec09c6b9a571992bfe5fa
umed1989 <32250251+umed1989@users.noreply.github.com>
2019-06-02 21:40:01 -0400

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,13 +1,4 @@
-# Artificial Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
 
 # Part 1 - Data Preprocessing
 
@@ -41,7 +32,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
+
 
 # Importing the Keras libraries and packages
 import keras
